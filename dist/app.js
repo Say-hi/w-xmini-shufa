55a3935b95ee2969903338b638bfb9f0c618c3d9
Say-hi <782108446@qq.com>
2020-02-25 00:44:49 -0600

    add hotReload

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,7 @@
  * @Author: Jiang WenQiang
  * @Date: 2019-09-01 10:29:30
  * @Last Modified by: Jiang WenQiang
- * @Last Modified time: 2020-01-12 21:02:37
+ * @Last Modified time: 2020-02-25 00:41:45
  */
 // /*eslint-disable*/
 var useUrl = require('./utils/service2');
@@ -1059,7 +1059,29 @@ App({
     });
     this.getBaseImageInfo();
   },
-  onShow: function onShow() {},
+  onShow: function onShow() {
+    // wx.getUpdateManager 在 1.9.90 才可用，请注意兼容
+    var updateManager = wx.getUpdateManager();
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate);
+    });
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否马上重启小程序？',
+        success: function success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+          }
+        }
+      });
+    });
+    updateManager.onUpdateFailed(function () {
+      // 新的版本下载失败
+    });
+  },
   onPageNotFound: function onPageNotFound() {
     wx.reLaunch({
       url: '/pages/index/index'
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
  * @Author: Jiang WenQiang
  * @Date: 2019-09-01 10:29:30
  * @Last Modified by: Jiang WenQiang
- * @Last Modified time: 2020-01-12 21:02:37
+ * @Last Modified time: 2020-02-25 00:42:17
  */
 // /*eslint-disable*/
 const useUrl = require('./utils/service2')
@@ -977,7 +977,29 @@ App({
     this.checkShare().then(res => this.su('ruler', res.data.data.ruler))
     this.getBaseImageInfo()
   },
-  onShow () {},
+  onShow () {
+// wx.getUpdateManager 在 1.9.90 才可用，请注意兼容
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+ // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否马上重启小程序？',
+        success: function (res) {
+          if (res.confirm) {
+       // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+ // 新的版本下载失败
+    })
+  },
   onPageNotFound () {
     wx.reLaunch({
       url: '/pages/index/index'
